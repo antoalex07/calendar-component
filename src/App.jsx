0a777adb41a19561bc7c2d35a4f7c14cdea538d9
antoalex07 <antoalex710@gmail.com>
2024-01-27 00:19:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Calendar from './components/Calendar'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Calendar</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div>Calendar Component</div>
+  )
+}
+
+export default Calendar
